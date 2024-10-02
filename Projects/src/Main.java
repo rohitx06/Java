5497718aff822ce06ae5d4f6aa572b0d223f599b
Rohit <rohitratheesh6@gmail.com>
2024-10-02 18:11:33 +0530

    Scanner functions

--- Projects/src/Main.java
+++ Projects/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner; //This is a code to import scanner in java which is used to take the user inputs.
+
 public class Main{
     public static void main(String[] args){
+        //Scanner tool creation
+        Scanner rohit = new Scanner(System.in);
 
-        System.out.println("Hello World ");
-        System.out.println("I Will seriously learn Java!!");
-        
+        //Printing write a message for the user
+
+        System.out.println("Write a message ...");
+        //Assigning the input given by the user to memory
+        String input = rohit.nextLine(); //nextLine() function is used to read the input given by the user till \n is reached
+        System.out.println(input);
+
 
     }
 }
